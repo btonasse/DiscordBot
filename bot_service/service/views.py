c5d32c9fe5e010bf78790d03d25dba2cdd0df828
Bernardo <btonasse@gmail.com>
2021-07-04 12:12:58 +0200

    initial config and migrate

--- bot_service/service/views.py
+++ bot_service/service/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+def welcome(request):
+    return Response('Hello there!')
