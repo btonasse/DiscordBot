30f7c335ab588734dfdf36fa888689bad5d7896a
Bernardo <btonasse@gmail.com>
2021-07-04 15:09:09 +0200

    player views

--- bot_service/service/urls.py
+++ bot_service/service/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('players', views.players, name='players')
+    path('players', views.players, name='players'),
+    path('players/<int:id>', views.player, name='player')
 ]
--- bot_service/service/views.py
+++ bot_service/service/views.py
@@ -19,3 +19,26 @@ def players(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def player(request, id: int):
+    try:
+        player = Player.objects.get(id=id)
+    except Player.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PlayerSerializer(player)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'PUT':
+        serializer = PlayerSerializer(player, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    else:
+        player.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
