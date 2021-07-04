c7476c4a5b55c08429e2bdab8015cbab399db508
Bernardo <btonasse@gmail.com>
2021-07-04 17:36:57 +0200

    bot as bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,14 +1,18 @@
 import os
 import discord
+from discord.ext import commands
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 SERVER = os.getenv('DISCORD_SERVER')
 
-class MyClient(discord.Client):
+
+class MyBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.my_guild = None
-    
+        self.my_server = None
+        self.add_command(self.hello)
+        
+
     async def on_ready(self):
         guild = discord.utils.get(self.guilds, name=SERVER)
         self.my_server = guild
@@ -18,6 +22,11 @@ class MyClient(discord.Client):
             f'{guild.name} - id: {guild.id}'
         )
 
+    @commands.command(name='hello', help='Just say hi.')
+    async def hello(ctx):
+        await ctx.send(f'Hello {ctx.author}')
 
-client = MyClient()
-client.run(TOKEN)
+
+if __name__ == '__main__':
+    bot = MyBot(command_prefix='!')
+    bot.run(TOKEN)
