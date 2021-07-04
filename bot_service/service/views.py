c1a519134be8f77f9f58d9fdd6bd198a0bf16fb8
Bernardo <btonasse@gmail.com>
2021-07-04 12:23:02 +0200

    register drf

--- bot_service/service/views.py
+++ bot_service/service/views.py
@@ -2,5 +2,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+@api_view(['GET'])
 def welcome(request):
-    return Response('Hello there!')
+    return Response('Hello there!', status=status.HTTP_200_OK)
