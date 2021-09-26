38be26c2dc0ba3573f63db57935126a5d84cfab9
btonasse <33516406+btonasse@users.noreply.github.com>
2021-09-26 02:55:05 +0200

    add AV3

--- mysite/api/jupiter/utils.py
+++ mysite/api/jupiter/utils.py
@@ -117,7 +117,7 @@ class MortemParser:
                 traits_count[trait] += 1
             data['traits'].append({'short_name': trait, 'order': len(data['traits'])+1, 'level': traits_count[trait]})
 
-        equipattern = re.compile(r'(?:^  Slot #|^  )(\d|Body|Head|Utility|Relic) +:( AV1| AV2| ENV)? ?(\S+(?: [^ \+ABP]+| AMP)*) ?([\+ABP\d]+)?\n((?:   \* )(?:.+\n)+)?', re.MULTILINE)
+        equipattern = re.compile(r'(?:^  Slot #|^  )(\d|Body|Head|Utility|Relic) +:( AV1| AV2| AV3| ENV)? ?(\S+(?: [^ \+ABP]+| AMP)*) ?([\+ABP\d]+)?\n((?:   \* )(?:.+\n)+)?', re.MULTILINE)
         equip_lines = re.findall(equipattern, self._mortem)
         data['equipment'] = []
         for line in equip_lines:
