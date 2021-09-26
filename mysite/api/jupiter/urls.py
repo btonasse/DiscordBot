c30d150b541cb772f41d2d8beec8e1978a773d0a
btonasse <33516406+btonasse@users.noreply.github.com>
2021-09-26 11:10:50 +0200

    add create from mortem and pagination

--- mysite/api/jupiter/urls.py
+++ mysite/api/jupiter/urls.py
@@ -3,5 +3,6 @@ from api.jupiter import views
 
 urlpatterns = [
     path('characters', views.CharactersView.as_view(), name='characters'),
-    path('characters/<int:id>', views.CharacterView.as_view(), name='character')
+    path('characters/<int:id>', views.CharacterView.as_view(), name='character'),
+    path('characters/mortem', views.CreateCharFromMortem.as_view(), name='new_char_from_mortem')
 ]
--- mysite/api/jupiter/views.py
+++ mysite/api/jupiter/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 import api.jupiter.models as md
 import api.jupiter.serializers as ser
+from api.jupiter.utils import MortemParser
 
 class CharactersView(generics.ListCreateAPIView):
     queryset = md.Character.objects.all()
@@ -10,3 +11,13 @@ class CharacterView(generics.RetrieveUpdateDestroyAPIView):
     queryset = md.Character.objects.all()
     serializer_class = ser.CharacterSerializer
     lookup_field = 'id'
+
+class CreateCharFromMortem(generics.CreateAPIView):
+    queryset = md.Character.objects.all()
+    serializer_class = ser.CharacterSerializer
+    def create(self, request, *args, **kwargs):
+        parser = MortemParser()
+        data_dict = parser.parse()
+        request.data.update(**data_dict)
+        return super().create(request, *args, **kwargs)
+
