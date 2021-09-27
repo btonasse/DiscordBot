b53f3f3b7de98343d3554119bac690952a3ead5f
btonasse <33516406+btonasse@users.noreply.github.com>
2021-09-27 15:59:54 +0200

    fix pagination

--- mysite/frontend/views/jupiter.py
+++ mysite/frontend/views/jupiter.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 import requests
-import json
 
 BASE_URL = 'http://127.0.0.1:8000'
 
 def index(request):
-    resp = requests.get(f"{BASE_URL}/api/jupiter/characters", params={'limit': 50})
-    context = {'characters': resp.json()}
+    resp = requests.get(f"{BASE_URL}/api/jupiter/characters")
+    data = resp.json()
+    p = Paginator(data, 50)
+    context = {
+        'characters': data,
+        'page_obj': p.get_page(request.GET.get('page'))
+        }
+    
     return render(request, 'frontend/jupiter/index.html', context)
