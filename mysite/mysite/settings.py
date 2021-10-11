b20587d2c9de06ab13c47992b8af7dc18ba868d4
btonasse <33516406+btonasse@users.noreply.github.com>
2021-10-11 21:21:35 +0200

    add debug toolbar

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'api.boardgames',
     'api.jupiter',
-    'frontend'
+    'frontend',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -128,3 +130,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
     path('', include('frontend.urls')),
     path('api/bg/', include('api.boardgames.urls')),
     path('api/jupiter/', include('api.jupiter.urls')),
     path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls))
 ]
