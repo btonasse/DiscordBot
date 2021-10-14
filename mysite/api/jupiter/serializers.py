3ce1e741c01ca43c8e8489cde1c6316ab610fd64
btonasse <33516406+btonasse@users.noreply.github.com>
2021-10-14 22:19:16 +0200

    optimize queries

--- mysite/api/jupiter/serializers.py
+++ mysite/api/jupiter/serializers.py
@@ -83,9 +83,9 @@ class CharacterEquipmentSerializer(serializers.ModelSerializer):
         fields = ['name', 'slot', 'rarity', 'mod_code', 'perks']
 
 class CharacterSerializer(serializers.ModelSerializer): 
-    klass = serializers.SlugRelatedField(queryset=md.Klass.objects.all(), slug_field='name')
-    killed_by = serializers.SlugRelatedField(queryset=md.Monster.objects.all(), slug_field='name', allow_null=True)
-    challenge = serializers.SlugRelatedField(queryset=md.Challenge.objects.all(), slug_field='name', allow_null=True)
+    klass = serializers.SlugRelatedField(read_only=True, slug_field='name')
+    killed_by = serializers.SlugRelatedField(read_only=True, slug_field='name', allow_null=True)
+    challenge = serializers.SlugRelatedField(read_only=True, slug_field='name', allow_null=True)
     awards = AwardSerializer(many=True, required=False)
     effects = EffectSerializer(many=True, required=False)
     traits = CharacterTraitSerializer(source='charactertrait_set', many=True)
--- mysite/api/jupiter/views.py
+++ mysite/api/jupiter/views.py
@@ -3,27 +3,29 @@ import api.jupiter.models as md
 import api.jupiter.serializers as ser
 from api.jupiter.utils import MortemParser
 
-class CharactersView(generics.ListCreateAPIView):
-    queryset = md.Character.objects.all().select_related(
+from django.db.models import Prefetch
+class BaseCharacterView(generics.GenericAPIView):
+    queryset = md.Character.objects.select_related(
         'killed_by', 'klass', 'challenge').prefetch_related(
-            'awards', 'effects', 'characterlocation_set__location', 'characterlocation_set__event', 'charactertrait_set__trait', 'characterkill_set__monster', 'characterinventory_set__item', 'characterequipment_set__equipmentperk_set__perk', 'characterequipment_set__equipment'
+            'awards',
+            'effects',
+            Prefetch('characterlocation_set', queryset=md.CharacterLocation.objects.select_related('location', 'event')),
+            Prefetch('charactertrait_set', queryset=md.CharacterTrait.objects.select_related('trait')),
+            Prefetch('characterkill_set', queryset=md.CharacterKill.objects.select_related('monster')),
+            Prefetch('characterinventory_set', queryset=md.CharacterInventory.objects.select_related('item')),
+            Prefetch('characterequipment_set', queryset=md.CharacterEquipment.objects.select_related('equipment').prefetch_related(
+                Prefetch('equipmentperk_set', queryset=md.EquipmentPerk.objects.select_related('perk'))
+            ))
         )
     serializer_class = ser.CharacterSerializer
+    
+class CharactersView(generics.ListCreateAPIView, BaseCharacterView):
+    pass
 
-class CharacterView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = md.Character.objects.all().select_related(
-        'killed_by', 'klass', 'challenge').prefetch_related(
-            'awards', 'effects', 'characterlocation_set__location', 'characterlocation_set__event', 'charactertrait_set__trait', 'characterkill_set__monster', 'characterinventory_set__item', 'characterequipment_set__equipmentperk_set__perk', 'characterequipment_set__equipment'
-        )
-    serializer_class = ser.CharacterSerializer
+class CharacterView(generics.RetrieveUpdateDestroyAPIView, BaseCharacterView):
     lookup_field = 'id'
 
-class CreateCharFromMortem(generics.CreateAPIView):
-    queryset = md.Character.objects.all().select_related(
-        'killed_by', 'klass', 'challenge').prefetch_related(
-            'awards', 'effects', 'characterlocation_set__location', 'characterlocation_set__event', 'charactertrait_set__trait', 'characterkill_set__monster', 'characterinventory_set__item', 'characterequipment_set__equipmentperk_set__perk', 'characterequipment_set__equipment'
-        )
-    serializer_class = ser.CharacterSerializer
+class CreateCharFromMortem(generics.CreateAPIView, BaseCharacterView):
     def create(self, request, *args, **kwargs):
         parser = MortemParser()
         data_dict = parser.parse()
