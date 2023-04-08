0d6b2b17fd952a85b9b9aeeb71df2b0a945f121c
btonasse <33516406+btonasse@users.noreply.github.com>
2023-04-08 22:56:38 +0200

    some autoformatting

--- mysite/api/jupiter/utils.py
+++ mysite/api/jupiter/utils.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from dataclasses import dataclass, asdict
 from typing import List
 
+
 @dataclass(init=False)
 class Mortem:
     name: str
@@ -32,100 +33,100 @@ class Mortem:
 
 
 class MortemParser:
-    '''
+    """
     Parses a mortem.txt file generated after a Jupiter Hell run finishes
-    '''
-    def __init__(self, filepath: str = '') -> None:
+    """
+
+    def __init__(self, filepath: str = "") -> None:
         if filepath:
             self._filepath = Path(filepath).resolve()
         else:
-            self._filepath = Path(os.getenv('JUPITER_MORTEM')).resolve()
+            self._filepath = Path(os.getenv("JUPITER_MORTEM")).resolve()
         self._mortem = None
         self._last_modified = None
         self._load_file()
         self.data = Mortem()
 
     def _load_file(self) -> str:
-        '''
+        """
         Load contents of mortem file onto self.mortem
         Also loads the file last modified timestamp
-        '''
+        """
         if not self._filepath.is_file():
             raise FileNotFoundError
-        if self._filepath.suffix != '.txt':
-            raise ValueError('Only txt files accepted.')
-        with self._filepath.open('r') as file:
+        if self._filepath.suffix != ".txt":
+            raise ValueError("Only txt files accepted.")
+        with self._filepath.open("r") as file:
             content = file.read()
         # Fix bug in mortem.txt for CRI grenadiers
-        self._mortem = content.replace('CRI  grenadiers', 'CRI grenadiers ')
+        self._mortem = content.replace("CRI  grenadiers", "CRI grenadiers ")
         self._last_modified = datetime.fromtimestamp(self._filepath.stat().st_mtime)
 
-
     def convert_to_singular(self, name: str) -> str:
-        '''
+        """
         Converts a name potentially in plural form to its singular form
-        '''
-        if name.endswith('i'):
-            name = name[:-1] + 'os'
-        elif name.endswith('oes'):
+        """
+        if name.endswith("i"):
+            name = name[:-1] + "os"
+        elif name.endswith("oes"):
             name = name[:-2]
-        elif name.endswith('ies'):
-            name = name[:-3] + 'y'
-        elif name.endswith('ae'):
+        elif name.endswith("ies"):
+            name = name[:-3] + "y"
+        elif name.endswith("ae"):
             name = name[:-1]
-        elif name.endswith('s') and not name.endswith('os'):
+        elif name.endswith("s") and not name.endswith("os"):
             name = name[:-1]
         return name
 
     def parse(self) -> dict:
-        '''
+        """
         Parses mortem file and populates self.data with all necessary datapoints to create a new character instance
-        '''
-        line1 = re.compile(r'(.+), level (\d+) (.+),')
+        """
+        line1 = re.compile(r"(.+), level (\d+) (.+),")
         self.data.name, self.data.level, self.data.klass = re.search(line1, self._mortem).groups()
         self.data.level = int(self.data.level)
 
-        line2 = re.compile(r'^(killed on|commited suicide on|defeated the Harbinger) (.+?)(?: by a (.+)\.|\.)$', re.MULTILINE)
+        line2 = re.compile(r"^(killed on|commited suicide on|defeated the Harbinger) (.+?)(?: by a (.+)\.|\.)$", re.MULTILINE)
         match = re.search(line2, self._mortem)
-        if match.groups()[0] == 'defeated the Harbinger':
+        if match.groups()[0] == "defeated the Harbinger":
             self.data.won = True
-            last_location = 'Dante Altar'
+            last_location = "Dante Altar"
             self.data.killed_by = None
-            
+
         else:
-            self.data.killed_by = match.groups()[2] or 'suicide'
+            self.data.killed_by = match.groups()[2] or "suicide"
             last_location = match.groups()[1]
             self.data.won = False
-            
-        line4 = re.compile(r'He survived for (\d+) turns.')
+
+        line4 = re.compile(r"He survived for (\d+) turns.")
         self.data.turns_survived = int(re.search(line4, self._mortem).groups()[0])
 
-        line5 = re.compile(r'The run time was (?:(\d+)h )?(\d+)m (\d+)s.')
-        runtime_list = [grp or '00' for grp in re.search(line5, self._mortem).groups()]
-        self.data.run_time = ':'.join(runtime_list)
+        line5 = re.compile(r"The run time was (?:(\d+)h )?(\d+)m (\d+)s.")
+        runtime_list = [grp or "00" for grp in re.search(line5, self._mortem).groups()]
+        self.data.run_time = ":".join(runtime_list)
 
-        line6 = re.compile(r'World seed was (\d+).')
+        line6 = re.compile(r"World seed was (\d+).")
         self.data.seed = int(re.search(line6, self._mortem).groups()[0])
 
-        line7 = re.compile(r'He scored (\d+) points.')
+        line7 = re.compile(r"He scored (\d+) points.")
         self.data.points = int(re.search(line7, self._mortem).groups()[0])
 
-        fatedpattern = re.compile(r'(Random Number Gods)')
+        fatedpattern = re.compile(r"(Random Number Gods)")
         self.data.fated = False
         if re.search(fatedpattern, self._mortem):
             self.data.fated = True
 
-        line8 = re.compile(r'(EASY|MEDIUM|HARD|ULTRAVIOLENT|NIGHTMARE)')
+        line8 = re.compile(r"(EASY|MEDIUM|HARD|ULTRAVIOLENT|NIGHTMARE)")
         self.data.difficulty = re.search(line8, self._mortem).groups()[0][0]
 
-        challengepat = re.compile(r'^He was a(?:(?:n )|(?: paragon of ))([\s\w]+)', re.MULTILINE)
+        challengepat = re.compile(r"^He was a(?:(?:n )|(?: paragon of ))([\s\w]+)", re.MULTILINE)
         challenge = re.search(challengepat, self._mortem)
         if challenge:
             self.data.challenge = challenge.groups()[0]
         else:
             self.data.challenge = None
 
-        locpattern = re.compile(r'^(\w+(?: \w+)*) -(.*)$', re.MULTILINE)
+        locpattern = re.compile(r"^(\w+(?: \w+)*) -(.*)$", re.MULTILINE)
         loc_groups = re.findall(locpattern, self._mortem)
         visited_locations = dict()
         order = 0
@@ -134,47 +135,47 @@ class MortemParser:
             right = grp[1].strip()
             if left not in visited_locations:
                 order += 1
-                visited_locations[left] = {'name': left, 'order': order, 'event': None}
-            if right.startswith('>'):
+                visited_locations[left] = {"name": left, "order": order, "event": None}
+            if right.startswith(">"):
                 order += 1
-                visited_locations[right[1:].strip()] = {'name': right[1:].strip(), 'order': order, 'event': None}
+                visited_locations[right[1:].strip()] = {"name": right[1:].strip(), "order": order, "event": None}
             else:
-                visited_locations[left]['event'] = right
+                visited_locations[left]["event"] = right
         if last_location and last_location not in visited_locations:
-            visited_locations[last_location] = {'name': last_location, 'order': order+1, 'event': None}
+            visited_locations[last_location] = {"name": last_location, "order": order + 1, "event": None}
         # Convert dict to list
         self.data.visited_locations = list(visited_locations.values())
 
-        awardpattern = re.compile(r'(?<=Awards\n).+(?=\nHe killed)', re.DOTALL)
+        awardpattern = re.compile(r"(?<=Awards\n).+(?=\nHe killed)", re.DOTALL)
         self.data.awards = []
         try:
             award_lines = re.search(awardpattern, self._mortem)[0].splitlines()
             for award in award_lines:
-                if not award.startswith('   *'):
-                    self.data.awards.append({'name': award.strip()})
-        except TypeError: #No matches
+                if not award.startswith("   *"):
+                    self.data.awards.append({"name": award.strip()})
+        except TypeError:  # No matches
             pass
 
-        effectspattern = re.compile(r'(?<=Permanents\n).+(?=\nInventory)', re.DOTALL)
+        effectspattern = re.compile(r"(?<=Permanents\n).+(?=\nInventory)", re.DOTALL)
         self.data.effects = []
         try:
             effects_lines = re.search(effectspattern, self._mortem)[0].splitlines()
             for effect in effects_lines:
-                self.data.effects.append({'name': effect.strip()})
-        except TypeError: #No matches
+                self.data.effects.append({"name": effect.strip()})
+        except TypeError:  # No matches
             pass
 
-        totenemies = re.compile(r'He killed \d+ out of (\d+) enemies.')
+        totenemies = re.compile(r"He killed \d+ out of (\d+) enemies.")
         self.data.total_enemies = int(re.search(totenemies, self._mortem).groups()[0])
-        
-        killpattern = re.compile(r'(?:^ |\s{3})(\d+)\s{1,2}(\w+(?: \w+)*)', re.MULTILINE)
+
+        killpattern = re.compile(r"(?:^ |\s{3})(\d+)\s{1,2}(\w+(?: \w+)*)", re.MULTILINE)
         kill_groups = re.findall(killpattern, self._mortem)
         self.data.kills = []
         for grp in kill_groups:
             monster_name = self.convert_to_singular(grp[1])
-            self.data.kills.append({'name': monster_name, 'howmany': grp[0]})
+            self.data.kills.append({"name": monster_name, "howmany": grp[0]})
 
-        traitpattern = re.compile(r'(?<=^  |->)*?(\w+)(?=->|\n\nEquipment)', re.MULTILINE)
+        traitpattern = re.compile(r"(?<=^  |->)*?(\w+)(?=->|\n\nEquipment)", re.MULTILINE)
         traits_list = re.findall(traitpattern, self._mortem)
         # Convert list to deserializable list of dicts
         self.data.traits = []
@@ -184,13 +185,13 @@ class MortemParser:
                 traits_count[trait] = 1
             else:
                 traits_count[trait] += 1
-            self.data.traits.append({'short_name': trait, 'order': len(self.data.traits)+1, 'level': traits_count[trait]})
+            self.data.traits.append({"short_name": trait, "order": len(self.data.traits) + 1, "level": traits_count[trait]})
 
-        equipattern = re.compile(r'(?:^  Slot #|^  )(\d|Body|Head|Utility|Relic) +:( AV1| AV2| AV3)? ?(\S+(?: [^ \+ABP]+| AMP)*) ?([\+ABPSC\d]+)?\n((?:   \* )(?:.+\n)+)?', re.MULTILINE)
+        equipattern = re.compile(r"(?:^  Slot #|^  )(\d|Body|Head|Utility|Relic) +:( AV1| AV2| AV3)? ?(\S+(?: [^ \+ABP]+| AMP)*) ?([\+ABPSC\d]+)?\n((?:   \* )(?:.+\n)+)?", re.MULTILINE)
         equip_lines = re.findall(equipattern, self._mortem)
         self.data.equipment = []
         for line in equip_lines:
-            if all(char in '0123456789' for char in line[0]):
+            if all(char in "0123456789" for char in line[0]):
                 slot = int(line[0])
             else:
                 slot = None
@@ -198,44 +199,46 @@ class MortemParser:
             equip_name = line[2]
             mod_code = line[3] or None
             # Build perk list
-            perks_raw = [perk.lstrip('   * ') for perk in line[4].split('\n') if perk] 
+            perks_raw = [perk.lstrip("   * ") for perk in line[4].split("\n") if perk]
             # Separte perk name and level
             perks = []
             for perk in perks_raw:
                 if perk:
                     split_name = perk.split()
                     try:
-                        level = int(split_name[-1].replace('%', ''))
-                        name = ' '.join(split_name[:-1])
+                        level = int(split_name[-1].replace("%", ""))
+                        name = " ".join(split_name[:-1])
                     except ValueError:
                         level = None
-                        name = ' '.join(split_name)
-                    perks.append({'name': name, 'level': level})
-            self.data.equipment.append({'name': equip_name, 'slot': slot, 'rarity': rarity, 'mod_code': mod_code, 'perks': perks})
+                        name = " ".join(split_name)
+                    perks.append({"name": name, "level": level})
+            self.data.equipment.append({"name": equip_name, "slot": slot, "rarity": rarity, "mod_code": mod_code, "perks": perks})
 
-        invpattern = re.compile(r'(?<=Inventory\n)(.+)', re.DOTALL)
+        invpattern = re.compile(r"(?<=Inventory\n)(.+)", re.DOTALL)
         inv_lines = re.search(invpattern, self._mortem).groups()[0].splitlines()
         inventory = dict()
         for line in inv_lines:
-            split_line_raw = line.split('(')
-            split_line = [item[1:].strip().replace(')','') for item in split_line_raw]
+            split_line_raw = line.split("(")
+            split_line = [item[1:].strip().replace(")", "") for item in split_line_raw]
             if not inventory.get(split_line[0]):
                 inventory[split_line[0]] = 0
             try:
                 inventory[split_line[0]] += int(split_line[1])
             except IndexError:
-                inventory[split_line[0]] += 1 # There is no count for this inventory line
+                inventory[split_line[0]] += 1  # There is no count for this inventory line
         # convert dict to deserializable format
-        self.data.inventory = [{'item': k, 'howmany': v} for k,v in inventory.items()]
+        self.data.inventory = [{"item": k, "howmany": v} for k, v in inventory.items()]
 
         # add file creation timestamp
-        self.data.mortem_timestamp = datetime.strftime(self._last_modified, '%Y-%m-%dT%H:%M:%S%z')
-        
+        self.data.mortem_timestamp = datetime.strftime(self._last_modified, "%Y-%m-%dT%H:%M:%S%z")
+
         return asdict(self.data)
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     import json
-    path = "D:\\SteamD\\steamapps\\common\\Jupiter Hell\\mortem.txt"
+
+    path = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Jupiter Hell\\mortem.txt"
     x = MortemParser(path)
     data = x.parse()
     print(json.dumps(data, indent=4))
--- mysite/api/jupiter/views.py
+++ mysite/api/jupiter/views.py
@@ -4,34 +4,38 @@ import api.jupiter.serializers as ser
 from api.jupiter.utils import MortemParser
 
 from django.db.models import Prefetch
+
+
 class BaseCharacterView(generics.GenericAPIView):
-    queryset = md.Character.objects.select_related(
-        'killed_by', 'klass', 'challenge').prefetch_related(
-            'awards',
-            'effects',
-            Prefetch('characterlocation_set', queryset=md.CharacterLocation.objects.select_related('location', 'event')),
-            Prefetch('charactertrait_set', queryset=md.CharacterTrait.objects.select_related('trait')),
-            Prefetch('characterkill_set', queryset=md.CharacterKill.objects.select_related('monster')),
-            Prefetch('characterinventory_set', queryset=md.CharacterInventory.objects.select_related('item')),
-            Prefetch('characterequipment_set', queryset=md.CharacterEquipment.objects.select_related('equipment').prefetch_related(
-                Prefetch('equipmentperk_set', queryset=md.EquipmentPerk.objects.select_related('perk'))
-            ))
-        )
+    queryset = md.Character.objects.select_related("killed_by", "klass", "challenge").prefetch_related(
+        "awards",
+        "effects",
+        Prefetch("characterlocation_set", queryset=md.CharacterLocation.objects.select_related("location", "event")),
+        Prefetch("charactertrait_set", queryset=md.CharacterTrait.objects.select_related("trait")),
+        Prefetch("characterkill_set", queryset=md.CharacterKill.objects.select_related("monster")),
+        Prefetch("characterinventory_set", queryset=md.CharacterInventory.objects.select_related("item")),
+        Prefetch("characterequipment_set", queryset=md.CharacterEquipment.objects.select_related("equipment").prefetch_related(Prefetch("equipmentperk_set", queryset=md.EquipmentPerk.objects.select_related("perk")))),
+    )
+
     def get_serializer_class(self):
-        if self.request._request.method == 'GET':
+        if self.request._request.method == "GET":
             return ser.CharSerializerReadonly
         return ser.CharacterSerializer
-    
+
+
 class CharactersView(generics.ListCreateAPIView, BaseCharacterView):
     pass
 
+
 class CharacterView(generics.RetrieveUpdateDestroyAPIView, BaseCharacterView):
-    lookup_field = 'id'
+    lookup_field = "id"
+
 
 class CreateCharFromMortem(generics.CreateAPIView, BaseCharacterView):
     def create(self, request, *args, **kwargs):
         parser = MortemParser()
         data_dict = parser.parse()
+        # request.data._mutable = True
         request.data.update(**data_dict)
+        # request.data._mutable = False
         return super().create(request, *args, **kwargs)
-
